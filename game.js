a2eebc9e6327773de9f82e730b1ef1bb4cfd5241
James Brill <jebssnooker@hotmail.com>
2014-01-05 00:13:24 +0000

    Merged improved AI with code for animations and menu.

--- game.js
+++ game.js
@@ -1,14 +1,25 @@
 var svgdoc = null;                          // SVG root document node
-var turn;
-var boardSize = 10;
+var boardHeight = 6;
+var boardWidth = 7;
 var gameBoard;
 var cellSize = 40;
 
 var fourInARowLine;
 var minimaxDepth = 2;
-var gameOver;
-var ready;
-var nextMove;
+
+var MAXIMISINGPLAYER = 1;
+var MINIMISINGPLAYER = -1;
+var maximisingPlayerString = "Computer";
+var minimisingPlayerString = "Human";
+var MINUTILITY = -100;
+var MAXUTILITY = 100;
+var gameOver = false;
+var maximisingPlayerBestMovesGrid = [];
+var minimisingPlayerBestMovesGrid = [];
+
+var ready = true;
+var turn = MAXIMISINGPLAYER;
+var nextMove = MINIMISINGPLAYER;
 var difficulty = 1;
 
 //loaded by the SVG document
@@ -26,39 +37,37 @@ function load(evt)
 
 function setUp()
 {	
-	gameOver = false;
-	nextMove = -1;
-	ready = true;
-	turn = 1;
-	gameBoard = new Array(boardSize);
-	for (var i = 0; i < boardSize; i++)
+	gameBoard = new Array(boardWidth);
+	for (var i = 0; i < boardWidth; i++)
 	{
-		gameBoard[i] = new Array(boardSize);
-		for(var j = 0; j < boardSize; j++)
+		gameBoard[i] = new Array(boardHeight);
+		for(var j = 0; j < boardHeight; j++)
 		{
 			gameBoard[i][j] = 0
 		}
 	}
+
 	var boardRect = svgdoc.createElementNS("http://www.w3.org/2000/svg", "rect");
 	boardRect.setAttribute("id", "board");
 	boardRect.setAttribute("stroke-width", "10");
 	boardRect.setAttribute("fill", "black");
 	boardRect.setAttribute("stroke", "black");	
-	boardRect.setAttribute("height", boardSize*cellSize);
-	boardRect.setAttribute("width", boardSize*cellSize);
+	boardRect.setAttribute("height", boardHeight*cellSize);
+	boardRect.setAttribute("width", boardWidth*cellSize);
 	var columnRect = svgdoc.createElementNS("http://www.w3.org/2000/svg", "rect");
 	columnRect.setAttribute("id", "column");
 	columnRect.setAttribute("stroke-width", "2");
 	columnRect.setAttribute("fill", "none");
 	columnRect.setAttribute("stroke", "red");	
-	columnRect.setAttribute("height", boardSize*cellSize);
+	columnRect.setAttribute("height", boardHeight*cellSize);
 	columnRect.setAttribute("width", cellSize);
 	columnRect.setAttribute("visibility", "hidden");
 	var main = svgdoc.getElementById("main");
 	main.appendChild(boardRect);
-	for(var i = 0; i < boardSize; i++)
+
+	for(var i = 0; i < boardWidth; i++)
 	{
-		for(var j = 0; j < boardSize; j++)
+		for(var j = 0; j < boardHeight; j++)
 		{
 			var cell = svgdoc.createElementNS("http://www.w3.org/2000/svg", "use");
 			cell.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#cell");
@@ -69,13 +78,16 @@ function setUp()
 			main.appendChild(cell);
 		}
 	}
+
+	initialiseBestMovesGrids();
+
 	main.appendChild(columnRect);
 	nextTurn();
 }
 
 function click(cell)
 {
-	if(nextMove == -1)
+	if(nextMove == MINIMISINGPLAYER)
 	{
 		var x = cell.getAttribute("x");
 		nextMove = x/cellSize;
@@ -85,6 +97,20 @@ function click(cell)
 	{
 		//alert("nextTurn");
 		//nextTurn();
+	}
+}
+
+function initialiseBestMovesGrids()
+{	
+	for (var x = 0; x < boardWidth; x++)
+	{
+		maximisingPlayerBestMovesGrid[x] = [];
+		minimisingPlayerBestMovesGrid[x] = [];
+		for (var y = 0; y < boardHeight; y++)
+		{
+			maximisingPlayerBestMovesGrid[x][y] = 0;
+			minimisingPlayerBestMovesGrid[x][y] = 0;
+		}
 	}
 }
 
@@ -134,14 +160,15 @@ function clickDifficulty(val)
 	{
 		case 3:
 			svgdoc.getElementById("dif3").setAttribute("fill","red");
-			difficulty++;
+			difficulty+=4;
 		case 2:
 			svgdoc.getElementById("dif2").setAttribute("fill","yellow");
-			difficulty++;
+			difficulty+=3;
 		case 1:
 			svgdoc.getElementById("dif1").setAttribute("fill","green");
-			difficulty++;
+			difficulty+=1;
 	}
+	minimaxDepth = difficulty;
 }
 
 function nextTurn()
@@ -150,19 +177,19 @@ function nextTurn()
 	{
 		return;
 	}
-	if(turn == -1)
+	if(turn == MINIMISINGPLAYER)
 	{
 		ready = false;
-		var computerMove = minimax(-1, gameBoard, minimaxDepth).move;
+		var computerMove = getComputerMove();
 		move(computerMove);
 	}
-	if(turn == 1)
+	if(turn == MAXIMISINGPLAYER)
 	{
-		if(nextMove != -1)
+		if(nextMove != MINIMISINGPLAYER)
 		{
 			ready = false;
 			move(nextMove);
-			nextMove = -1;
+			nextMove = MINIMISINGPLAYER;
 		}
 		else
 		{
@@ -171,25 +198,17 @@ function nextTurn()
 	}
 }
 
-function move(row)
+function move(column)
 {
 	var main = svgdoc.getElementById("main");
-	var x = row;	
+	var x = column;	
 	var y = nextFree(x, gameBoard);	
 	if(y != -1)
 	{
 		gameBoard[x][y] = turn;		
 		var counter = svgdoc.createElementNS("http://www.w3.org/2000/svg", "use");
-		if(turn == 1)
-		{
-			counter.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#counter1");
-			turn = -1;
-		}
-		else
-		{
-			counter.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#counter2");
-			turn = 1;
-		}
+		counter.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#counter" + turn);
+		turn = -turn;
 		counter.setAttribute("x",x*cellSize);
 		counter.setAttribute("y",y*cellSize);
 		
@@ -215,39 +234,112 @@ function move(row)
 function checkWin(x,y)
 {
 	var line = isLine(x,y,gameBoard);
-        if(line != 0 && fourInARowLine != null)
-        {
-                while (fourInARowLine.length>0)
-                {
-                        var lineX = fourInARowLine[0].pop();
-                        var lineY = fourInARowLine[1].pop();
-                        if(lineX == null || lineY == null)
-                        {
-                                break;
-                        }
-                        var main = svgdoc.getElementById("main");
-                        var winCounter = svgdoc.createElementNS("http://www.w3.org/2000/svg", "use");
-                        winCounter.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#counter3");
-                        winCounter.setAttribute("x",lineX*cellSize);
-						winCounter.setAttribute("y",lineY*cellSize);
-						main.appendChild(winCounter);
-                }
-                gameOver = true;
-                var winnerString = (turn == 1) ? "COMPUTER" : "HUMAN";
-                alert(winnerString + " WINS!");
-        }
+    if(line != 0 && fourInARowLine != null)
+    {
+            while (fourInARowLine.length>0)
+            {
+                    var lineX = fourInARowLine[0].pop();
+                    var lineY = fourInARowLine[1].pop();
+                    if(lineX == null || lineY == null)
+                    {
+                            break;
+                    }
+                    var main = svgdoc.getElementById("main");
+                    var winCounter = svgdoc.createElementNS("http://www.w3.org/2000/svg", "use");
+                    winCounter.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#counter3");
+                    winCounter.setAttribute("x",lineX*cellSize);
+					winCounter.setAttribute("y",lineY*cellSize);
+					main.appendChild(winCounter);
+            }
+            gameOver = true;
+            var winnerString = (turn == MAXIMISINGPLAYER) ? maximisingPlayerString : minimisingPlayerString;
+            alert(winnerString + " wins.");
+    }
+    if (isBoardFull(gameBoard) && !gameOver)
+    {
+    	alert("Draw.");
+	}
    	ready = true;
     nextTurn();
 }
 
+function getComputerMove()
+{
+	var possibleWinningMoveForCurrentPlayer = getWinningMove(turn, gameBoard);
+	var possibleWinningMoveForOtherPlayer = getWinningMove(-turn, gameBoard);
+	if (possibleWinningMoveForCurrentPlayer != -1)
+	{
+		return possibleWinningMoveForCurrentPlayer;
+	}
+	if (possibleWinningMoveForOtherPlayer != -1)
+	{
+		return possibleWinningMoveForOtherPlayer;
+	}
+	var alphabetaMove = alphabeta(turn, gameBoard, minimaxDepth, -1000, 1000).move;
+
+	var potentiallyBetterMove;
+	if (isSurrenderingMove(alphabetaMove, turn))
+	{
+		potentiallyBetterMove = getNonSurrenderingMove(turn);
+		return (potentiallyBetterMove == -1) ? alphabetaMove : potentiallyBetterMove;
+	}
+	return alphabetaMove;
+} 
+
+function isSurrenderingMove(move, turn)
+{
+	var tempBoard = clone2DArray(gameBoard);
+	var y = nextFree(move, tempBoard);
+	if (y == -1)
+	{
+		return false;
+	}
+	tempBoard[move][y] = turn;
+	return (getWinningMove(-turn, tempBoard) != -1);
+}
+
+function getNonSurrenderingMove(player)
+{
+	for(var x = 0; x < boardWidth; x++)
+	{
+		var y = nextFree(x, gameBoard);
+		if (y == -1)
+		{
+			continue;
+		}
+		var possibleNonSurrenderingState = clone2DArray(gameBoard);
+		possibleNonSurrenderingState[x][y] = player;
+		if(getWinningMove(-turn, possibleNonSurrenderingState) == -1)
+		{			
+			return x;
+		}
+	}
+	return -1;	
+}
+
+function getWinningMove(player, board)
+{
+	for(var x = 0; x < boardWidth; x++)
+	{
+		var y = nextFree(x, board);
+		if (y == -1)
+		{
+			continue;
+		}
+		var possibleWinningState = clone2DArray(board);
+		possibleWinningState[x][y] = player;
+		if(isLine(x, y, possibleWinningState) == player)
+		{			
+			return x;
+		}
+	}
+	return -1;
+}
+
 function nextFree(x, board)
 {
-	for(var y = boardSize -1; y >= 0; y--)
+	for(var y = boardHeight -1; y >= 0; y--)
 	{
-		if (board[x] == undefined)
-		{
-			var temp = 5;
-		}
 		if(board[x][y] == 0)
 		{
 			return y;
@@ -261,7 +353,7 @@ function isLine(x,y,board)
         var winner = 0;
         var pos = new Array(new Array(),new Array());
         //horizontal
-        for(var i = 0; i < boardSize; i++)
+        for(var i = 0; i < boardWidth; i++)
         {
                 var piece = board[i][y];
                 if(piece != winner)
@@ -279,9 +371,8 @@ function isLine(x,y,board)
                 }
         }
         winner = 0;
-        count = 0;
         //vertical
-        for(var j = 0; j < boardSize; j++)
+        for(var j = 0; j < boardWidth; j++)
         {
                 var piece = board[x][j];
                 if(piece != winner)
@@ -299,12 +390,11 @@ function isLine(x,y,board)
                 }
         }
         winner = 0;
-        count = 0;
-        //diag right
+        //diag right and down
         var val = Math.min(x,y);
         var i = x - val;
         var j = y - val;
-        while(i < boardSize && j < boardSize)
+        while(i < boardWidth && j < boardHeight)
         {
                 var piece = board[i][j];
                 if(piece != winner)
@@ -324,12 +414,11 @@ function isLine(x,y,board)
                 j++;
         }
         winner = 0;
-        count = 0;
-        //diag left
-        val = Math.min((boardSize-(x+1)),y)
+        //diag left and down
+        val = Math.min((boardWidth-(x+1)),y)
         i = x + val;
         j = y - val;
-        while(i >= 0 && j < boardSize)
+        while(i >= 0 && j < boardHeight)
         {
                 var piece = board[i][j];
                 if(piece != winner)
@@ -351,103 +440,160 @@ function isLine(x,y,board)
         return 0;
 }
 
+function isBoardFull(board)
+{
+	for (var i = 0; i < boardWidth; i++)
+	{
+		if (nextFree(i, board) != -1)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+function isGameOver(board)
+{
+	if (isBoardFull(board))
+	{
+		return true;
+	}
+
+	for(var x = 0; x < boardWidth; x++)
+	{
+		var y = nextFree(x, board);
+		if (y < boardHeight - 1)
+		{
+			y++;
+		}
+
+		if(isLine(x, y, board) != 0)
+		{			
+			return true;
+		}
+	}
+	return false;
+}
+
 // A red three-in-a-row is defined as an empty position on the board that would win the game
 // for red if a red counter were to be placed there. The more of these, the higher the heuristic 
 // value. Yellow three-in-a-rows are subtracted. Return maximum or minimum utility if the 
 // state is a victory state.
-function threeInARowHeuristic(state)
+function threeInARowHeuristic(state, turn)
 {
-	var threeInARows = 0;
-	for (var i = 0; i < boardSize; i++)
-	{
-		for (var j = 0; j < boardSize; j++)
-		{		
-			// State is a human victory - minimum utility
-			if (isLine(i, j, state) == 1) 
-			{
-				return -100;
-			}
+    var humanThreeInARows = 0;
+    var machineThreeInARows = 0;
+    for (var i = 0; i < boardWidth; i++)
+    {        
+        var y = nextFree(i, state);
+        if (y == -1)
+        {
+            continue;
+        }
 
-			// State is a machine victory - maximum utility
-			if (isLine(i, j, state) == -1) // red/computer
-			{
-				return 100;
-			}
+        var topCounterIndex = y;
+        if (y < boardHeight - 1)
+        {
+            topCounterIndex++;
+        }
 
-			if (state[i][j] == 0)
-			{
-				state[i][j] = 1; // yellow/human
-				if (isLine(i, j, state) == 1)
-				{
-					threeInARows--;
-				}
-				state[i][j] = -1; // red/computer
-				if (isLine(i, j, state) == -1)
-				{
-					threeInARows++;
-				}
-				state[i][j] = 0;
-			}
-		}
-	}
-	return threeInARows;
+        // State is a guaranteed human victory - minimum utility
+        if (isLine(i, topCounterIndex, state) == MINIMISINGPLAYER) 
+        {
+            return MINUTILITY;
+        }
+
+        // State is a guaranteed machine victory - maximum utility
+        if (isLine(i, topCounterIndex, state) == MAXIMISINGPLAYER) 
+        {
+            return MAXUTILITY;
+        }
+
+        if (state[i][y] == 0)
+        {
+            state[i][y] = MINIMISINGPLAYER; // yellow/human
+            if (isLine(i, y, state) == MINIMISINGPLAYER)
+            {
+                humanThreeInARows++;
+            }
+            state[i][y] = MAXIMISINGPLAYER; // red/computerOne
+            if (isLine(i, y, state) == MAXIMISINGPLAYER)
+            {
+                machineThreeInARows++;
+            }
+            state[i][y] = 0;
+        }
+    }
+
+    if (turn == MAXIMISINGPLAYER)
+    {
+        if (machineThreeInARows >= 1)
+        {
+            return MAXUTILITY;
+        }
+        if (humanThreeInARows >= 2)
+        {
+            return MINUTILITY;
+        }
+    }
+    else
+    {
+        if (humanThreeInARows >= 1)
+        {
+            return MINUTILITY;
+        }
+        if (machineThreeInARows >= 2)
+        {
+            return MAXUTILITY;
+        }
+    }
+    return machineThreeInARows - humanThreeInARows;
 }
 
-function minimax(turn, boardInstance, depth)
+function alphabeta(turn, boardInstance, depth, alpha, beta)
 {
-	// Eventually need to consider case when board is full
-
-	if (depth == 0)
+	if (depth == 0 || isGameOver(boardInstance))
 	{
-		return {heuristic: threeInARowHeuristic(boardInstance)};
+		return {heuristic: threeInARowHeuristic(boardInstance, turn)};
 	}
 
-	var moveHeuristicPairs = new Array();
-	for (var i = 0; i < boardSize; i++)
+	var bestMove = -1;
+	var moveOrder = sortBestMoves(boardInstance, turn);
+	for (var i = 0; i < boardWidth; i++)
 	{
-		var y = nextFree(i, boardInstance);
+		var x = moveOrder[i];
+		var y = nextFree(x, boardInstance);
 		if (y == -1)
 		{
 			continue;
 		}
 		var boardInstanceClone = clone2DArray(boardInstance)
-		boardInstanceClone[i][y] = turn;
-		var moveHeuristicPair = minimax(-turn, boardInstanceClone, depth - 1);
-		moveHeuristicPairs[i] = moveHeuristicPair;
-	}
-
-	// If computer's turn, maximise heuristic
-	if (turn == -1)
-	{
-		var max = -1000;
-		var bestMove = -1;
-		for (var i = 0; i < moveHeuristicPairs.length; i++)
+		boardInstanceClone[x][y] = turn;
+		var moveHeuristicPair = alphabeta(-turn, boardInstanceClone, depth - 1, alpha, beta);
+		if (turn == MAXIMISINGPLAYER) 
 		{
-			if (moveHeuristicPairs[i] != undefined && moveHeuristicPairs[i].heuristic > max)
+			if (moveHeuristicPair.heuristic > alpha)
 			{
-				max = moveHeuristicPairs[i].heuristic;
-				bestMove = i;
+				bestMove = x;
+				alpha = moveHeuristicPair.heuristic;
+			}		
+		}
+		else
+		{
+			if (moveHeuristicPair.heuristic < beta)
+			{
+				bestMove = x;
+				beta = moveHeuristicPair.heuristic;
 			}
 		}
-		return {heuristic: max, move: bestMove};
-	}
-	// If humans's turn, minimise heuristic
-	else
-	{
-		var min = 1000;
-		var bestMove = -1;
-		for (var i = 0; i < moveHeuristicPairs.length; i++)
+		if (beta <= alpha)
 		{
-			if (moveHeuristicPairs[i] != undefined && moveHeuristicPairs[i].heuristic < min)
-			{
-				min = moveHeuristicPairs[i].heuristic;
-				bestMove = i;
-			}
+			break;
 		}
-		return {heuristic: min, move: bestMove};
 	}
 
-	return {heuristic: -1000, move: -1};
+	var heuristicValue = (turn == MAXIMISINGPLAYER) ? alpha : beta;
+	return {heuristic: heuristicValue, move: bestMove};
 }
 
 function clone2DArray(array)
@@ -459,4 +605,88 @@ function clone2DArray(array)
 	    newArray[i] = array[i].slice(0);
     }
     return newArray;
+}
+
+// BORROWED FROM THE INTERNET
+function shuffle(array) 
+{
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function updateBestMoves(board, depth, turn, x) {
+    var y = nextFree(x, board);
+    if (turn == MAXIMISINGPLAYER) 
+    {
+        maximisingPlayerBestMovesGrid[x][y] = maximisingPlayerBestMovesGrid[x][y] + Math.pow(2, depth);
+    } 
+    else 
+    {
+        minimisingPlayerBestMovesGrid[x][y] = minimisingPlayerBestMovesGrid[x][y] + Math.pow(2, depth);
+    }
+}
+
+function sortBestMoves(board, turn) 
+{
+    var sortedArray = [];
+    var tempBestMoves = (turn == MAXIMISINGPLAYER) ? clone2DArray(maximisingPlayerBestMovesGrid)
+    											   : clone2DArray(minimisingPlayerBestMovesGrid);
+    for (var i = 0; i < boardWidth; i++) 
+    {
+        tempBestMoves[i] = tempBestMoves[i][nextFree(i, board)];
+        tempBestMoves[i] = (tempBestMoves == undefined) ? 0 : tempBestMoves[i];
+    }
+
+    var noBestMove = true;
+    for (var i = 0; i < tempBestMoves.length; i++)
+    {
+    	if (tempBestMoves[i] != 0)
+    	{
+    		noBestMove = false;
+    		break;
+    	}
+    }
+
+    if (noBestMove) 
+    {
+    	var randomPermutation = [];
+	    for (var i = 0; i < boardWidth; i++) 
+	    {
+	        randomPermutation[i] = i;
+	    }
+        return shuffle(randomPermutation);
+    }
+    
+    for (var i = 0; i < boardWidth; i++) 
+    {
+        var maxValue = -1;
+        var maxIndex = 0;
+        for (var x = 0; x < boardWidth; x++) 
+        {
+            if (tempBestMoves[x] > maxValue) 
+            {
+                maxValue = tempBestMoves[x];
+                maxIndex = x;
+            }
+        }
+        sortedArray[i] = maxIndex;
+        tempBestMoves[maxIndex] = -1000;    
+    }
+    return sortedArray;
 }
